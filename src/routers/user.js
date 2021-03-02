46acaf0ecb5b1ec94c81bfeffdb1f01e117fbfd2
Raihanul Haque <raihan.hd.500@gmail.com>
2021-03-02 15:56:15 +0600

    routes added to upload,delete and get avatar of user using multer and sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth');
+const multer = require('multer');
+const sharp = require('sharp');
 
 // register new users to database
 router.post('/signup', async (req, res) => {
@@ -49,6 +51,54 @@ router.post('/logoutAll', auth, async (req, res) => {
         res.send();
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+// create multer instance
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image!'))
+        }
+
+        cb(undefined, true);
+    }
+})
+
+// post avatar to user data
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 500, height: 500 }).png().toBuffer();
+
+    req.user.avatar = buffer;
+    await req.user.save()
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+// delete avatar from user
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+})
+
+// fetch user avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send()
     }
 })
 
