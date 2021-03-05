12786610c6b38b4db88079e41541b674320c2527
Raihanul Haque <raihan.hd.500@gmail.com>
2021-03-06 00:01:50 +0600

    user data method updated to include token in response

--- src/models/user.js
+++ src/models/user.js
@@ -77,9 +77,6 @@ userSchema.methods.toJSON = function () {
     const userObject = user.toObject()
 
     delete userObject.password;
-    delete userObject.tokens;
-    // delete userObject.avatar;
-
     return userObject;
 }
 
